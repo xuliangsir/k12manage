443406de62a064c353b1f9acf1de1691ceffaf82
sunweiqi <921939025@qq.com>
2017-06-22 18:37:43 +0800

    update

--- src/components/functions/kindergarten.vue
+++ src/components/functions/kindergarten.vue
@@ -7,14 +7,13 @@
 
         <div class="line">
             <el-table :data="tableData" stripe style="width: 100%" ref="multipleTable">
-                <el-table-column prop="date" label="预览图" width="180">
-                    <template scope="scope">
-                        <img :src="scope.row.url" class="preImg">
-                    </template>
+                <el-table-column prop="id" label="学校ID" width="100">
                 </el-table-column>
-                <el-table-column prop="url" label="地址">
+                <el-table-column prop="name" label="校名">
                 </el-table-column>
-                <el-table-column label="操作">
+                <el-table-column prop="description" label="描述">
+                </el-table-column>
+                <el-table-column label="操作" width="150">
                     <template scope="scope">
                         <el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
                         <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
@@ -35,17 +34,12 @@
         <el-dialog title="添加条目" :visible.sync="dialogVisible" :before-close="handleClose" class="dialogSzie">
 
             <el-form ref="form" :model="form" label-width="80px">
-                <el-form-item label="活动名称">
+                <el-form-item label="校名">
                     <el-input v-model="form.name"></el-input>
                 </el-form-item>
-                <el-form-item label="活动名称">
-                    <el-input v-model="form.name"></el-input>
-                </el-form-item>
-                <el-form-item label="活动名称">
-                    <el-input v-model="form.name"></el-input>
-                </el-form-item>
-                <el-form-item label="活动名称">
-                    <el-input v-model="form.name"></el-input>
+                <el-form-item label="描述">
+                    <el-input type="textarea" :autosize="{ minRows: 4, maxRows: 10}" placeholder="请输入内容" v-model="form.description">
+                    </el-input>
                 </el-form-item>
             </el-form>
 
@@ -65,8 +59,40 @@
         name: 'picture',
         data() {
             return {
-                tableData: [],
+                tableData: [{
+                        "id": 1,
+                        "name": "彩虹幼儿园_1",
+                        "description": "xuliang test_1"
+                    },
+                    {
+                        "id": 2,
+                        "name": "彩虹幼儿园_2",
+                        "description": "xuliang test_2"
+                    },
+                    {
+                        "id": 3,
+                        "name": "彩虹幼儿园_3",
+                        "description": "xuliang test_3"
+                    },
+                    {
+                        "id": 4,
+                        "name": "彩虹幼儿园_4",
+                        "description": "xuliang test_4"
+                    },
+                    {
+                        "id": 5,
+                        "name": "彩虹幼儿园_5",
+                        "description": "xuliang test_5"
+                    },
+                    {
+                        "id": 6,
+                        "name": "彩虹幼儿园_6",
+                        "description": "xuliang test_6"
+                    },
+                ],
                 dialogVisible: false,
+                dialogUsage: 0, // 0 新增 1编辑
+                dialogIndex: '',
 
 
 
@@ -75,7 +101,8 @@
                 count: 100,
 
                 form: {
-                    name: 'tiger'
+                    name: '',
+                    description: ''
                 }
 
             }
@@ -91,14 +118,15 @@
             },
             getData() {
                 this.$http({
-                    method: 'post',
+                    method: 'get',
                     url: this.$getApi + '/api/kindergartens',
-                    params: {
-                        "description": "test",
-                        "name": "xuliang"
-                    },
+                    // params: {
+                    //     "description": "test",
+                    //     "name": "xuliang"
+                    // },
                     headers: {
-                        Authorization: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwidXNlcm5hbWUiOiJmZW5naHVhLndhbmciLCJ0eXBlIjoiTUFOQUdFUiIsIm1vYmlsZSI6IjEzNzE2MTQ5NDQzIiwiZXhwIjoxNDk5NjcxMTE1fQ.QpQ6mNS60DMLgfoEyq9RO4IlvRgvgi1lrZa7uNSu94_OefI6Swft9TMAKSpzQJ0nL6SNJpPmHnI8spOCQ6xosg'
+                        'content-type': "multipart/form-data",
+                        'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwidXNlcm5hbWUiOiJmZW5naHVhLndhbmciLCJ0eXBlIjoiTUFOQUdFUiIsIm1vYmlsZSI6IjEzNzE2MTQ5NDQzIiwiZXhwIjoxNDk5NjcxMTE1fQ.QpQ6mNS60DMLgfoEyq9RO4IlvRgvgi1lrZa7uNSu94_OefI6Swft9TMAKSpzQJ0nL6SNJpPmHnI8spOCQ6xosg'
 
                     }
                 }).then((res) => { // promise对象
@@ -121,21 +149,44 @@
             },
             showDialog(path) {
                 // this.path = path
+
+                this.form.name = ''
+                this.form.description = ''
+
                 this.dialogVisible = true
+                this.dialogUsage = 0
+
             },
             handleCurrentChange(val) {
                 this.current_page = val
             },
             handleEdit(index, row) {
-                this.dialogVisible = true
 
-                // this.objData = Object.assign({}, row)
+                console.log(index)
+
+                this.dialogVisible = true
+                this.dialogUsage = 1
+                this.dialogIndex = index
+
+                this.form = Object.assign({}, row)
             },
             handleDelete(index, row) {
-
+                this.tableData.splice(index, 1)
             },
             isAdd(tag) { // 0 添加    1 取消
                 this.dialogVisible = false
+
+                if (tag === 0 && this.dialogUsage == 0) {
+                    this.tableData.push(this.form)
+                }
+
+                if (tag === 0 && this.dialogUsage == 1) {
+                    this.tableData[this.dialogIndex].name = this.form.name
+                    this.tableData[this.dialogIndex].description = this.form.description
+                }
+
+                this.form = {}
+
             },
             addItem() {
 
@@ -174,7 +225,6 @@
             this.getData()
         }
     }
-
 </script>
 <style scoped>
     /*#mainContent {*/
@@ -216,5 +266,4 @@
         height: auto;
         margin: 0 auto;
     }
-
 </style>
--- src/unit/config.js
+++ src/unit/config.js
@@ -1,7 +1,7 @@
 export default {
     install(Vue, options) {
         // Vue.prototype.$getApi = process.env.NODE_ENV === 'build' ? 'http://q.socap1.com:60405' : 'http://10.16.40.94:60405'
-        //  Vue.prototype.$getApi =  'http://q.socap1.com:60405'
+        // Vue.prototype.$getApi = 'http://q.socap1.com:60405'
         Vue.prototype.$getApi = 'http://10.16.40.71:8080'
     }
 }
